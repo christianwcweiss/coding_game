81a6d3a725ea3a989ce8e4498ffbe4bec0f866bb
Christian Weiss <christian.wc.weiss@bmw.de>
2023-12-18 22:18:06 +0100

    CodingGame number_partition solution
    Signed-off-by: Christian Weiss <christian.wc.weiss@bmw.de>

--- coding_game/easy/number_partition/main.py
+++ coding_game/easy/number_partition/main.py
@@ -4,4 +4,19 @@ from typing import List
 class Solution:
     @staticmethod
     def solve(n: int) -> List[str]:
-         pass
+        result = set()
+        st = [([n-i], n-(n-i)) for i in range(n)]
+        while st:
+            l, remaining = st.pop()
+            if remaining < 0:
+                continue
+            elif remaining == 0:
+                result.add(" ".join(map(str, l)))
+            else:
+                for i in range(1, remaining + 1):
+                    if i <= l[-1]:
+                        st.append((l + [i], remaining - i))
+
+        return sorted(result, reverse=True)
+
+
